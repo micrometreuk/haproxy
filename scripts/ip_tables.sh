f7eb625d95b7d7ba905645f478ad4f98d0ee8262
henok <henokwehibe@gmail.com>
2019-08-06 07:02:31 +0100

    auto open browser

--- scripts/ip_tables.sh
+++ scripts/ip_tables.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo iptables -t nat -I PREROUTING -i eth0 -p TCP -d 178.62.11.233/32 --dport 80 -j DNAT --to-destination 10.141.39.237:80
-sudo iptables -t nat -I PREROUTING -i eth0 -p TCP -d 178.62.11.233/32 --dport 443 -j DNAT --to-destination 10.141.39.237:443
+sudo iptables -t nat -I PREROUTING -i eth0 -p TCP -d 178.62.11.23/32 --dport 80 -j DNAT --to-destination 10.141.39.237:80
+sudo iptables -t nat -I PREROUTING -i eth0 -p TCP -d 178.62.11.23/32 --dport 443 -j DNAT --to-destination 10.141.39.237:443
