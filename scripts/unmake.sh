f2903186278b41dd3a7879ff55f811ab4d075712
henok <henokwehibe@gmail.com>
2019-08-14 11:19:33 +0100

    updating unmake destroy

--- scripts/unmake.sh
+++ scripts/unmake.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 #stop all contaners
-lxc list | awk '{print $2}' | xargs | cut -c 5- | xargs lxc stop
+#lxc list | awk '{print $2}' | xargs | cut -c 5- | xargs lxc stop
 #delete all contaners
-lxc list | awk '{print $2}' | xargs | cut -c 5- | xargs lxc delete
+#lxc list | awk '{print $2}' | xargs | cut -c 5- | xargs lxc delete
+lxc list | grep lb | awk '{print $2}'| xargs lxc stop
+lxc list | grep web1 | awk '{print $2}'| xargs lxc stop
+lxc list | grep web2 | awk '{print $2}'| xargs lxc stop
+lxc list | grep lb | awk '{print $2}'| xargs lxc delete
+lxc list | grep web1 | awk '{print $2}'| xargs lxc delete
+lxc list | grep web2 | awk '{print $2}'| xargs lxc delete
