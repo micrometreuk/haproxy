2f98300d27fcdef931d17b05635b68660bf91d31
henok <henokwehibe@gmail.com>
2019-08-20 10:06:16 +0100

    unmake-demo function update

--- scripts/unmake.sh
+++ scripts/unmake.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 #stop all contaners
-#lxc list | awk '{print $2}' | xargs | cut -c 5- | xargs lxc stop
-#delete all contaners
-#lxc list | awk '{print $2}' | xargs | cut -c 5- | xargs lxc delete
-lxc list | grep lb | awk '{print $2}'| xargs lxc stop
-lxc list | grep web1 | awk '{print $2}'| xargs lxc stop
-lxc list | grep web2 | awk '{print $2}'| xargs lxc stop
-lxc list | grep lb | awk '{print $2}'| xargs lxc delete
-lxc list | grep web1 | awk '{print $2}'| xargs lxc delete
-lxc list | grep web2 | awk '{print $2}'| xargs lxc delete
+unmake-demo () {
+lxc list | grep 'lb1\|web1\|web2'   | awk '{print $2}'| xargs lxc stop
+lxc list | grep 'lb1\|web1\|web2'   | awk '{print $2}'| xargs lxc delete
+}
+unmake-demo
