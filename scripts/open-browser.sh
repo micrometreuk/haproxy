548371221f3a3b87e0c87460c2ae47f82c90a5c5
henok <henokwehibe@gmail.com>
2019-08-25 21:28:37 +0100

    adjusting firefox

--- scripts/open-browser.sh
+++ scripts/open-browser.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-#lxc list | grep lb1 | awk '{print "http://"$6":32700"}' | xargs xdg-open
-echo  "pres CTRL + C to continue with  the terminal the firefox will remain open"
+open_broser () {
+echo  "$(tput setaf 1) $(tput setab 7) "pres CTRL + C to continue with the terminal, firefox will remain open " $(tput sgr 0)"
 lxc list | grep lb1 | awk '{print "http://"$6":32700"   }' | xargs nohup firefox --new-window & 
 lxc list | grep lb1 | awk '{print "http://"$6}' | xargs nohup firefox  & 
+
+}
+
+open_broser
